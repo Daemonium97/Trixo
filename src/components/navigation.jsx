947eb1ccfa1a0f42aa6d844504e8a215466d6d88
Daemonium97 <daemonium97@gmail.com>
2021-07-24 22:45:30 +0000

    styling logo

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -20,8 +20,9 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
             <span className='icon-bar'></span>{' '}
           </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            Rammstein
+          <a className='navbar-brand page-scroll' href='#page-top' id="trixo">
+            Trixo
+            
           </a>{' '}
         </div>
 
